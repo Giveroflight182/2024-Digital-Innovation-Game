d8d93c17c8cb32aa805338db5878a521f28064a7
Giveroflight182 <161523294+Giveroflight182@users.noreply.github.com>
2024-03-22 10:00:36 +1300

    River

---
+++ Assets/TreeSpawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawn : MonoBehaviour
+{
+
+    public GameObject tree;
+    public int MaxHeight = 1;
+    public int MinHeight = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
