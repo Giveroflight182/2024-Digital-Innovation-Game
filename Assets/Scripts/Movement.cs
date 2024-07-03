2bc7f8639240d77139218448655dcfe90924f9c5
TamsynGill <tamsyngill3@students.freyberg.ac.nz>
2024-07-04 11:47:51 +1200

    streetlights

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+//This script allows the player to control the character's movement and camera direction
 public class Movement : MonoBehaviour
 {
+    //Controls how fast the player is able to walk, run and jump etc. Also controls the range of the camera
     public Camera playerCamera;
     public float walkSpeed = 20f;
     public float runSpeed = 30f;
@@ -15,15 +17,15 @@ public class Movement : MonoBehaviour
     public float defaultHeight = 2f;
     public float crouchHeight = 1f;
     public float crouchSpeed = 3f;
-
     private Vector3 moveDirection = Vector3.zero;
     private float rotationX = 0;
     private CharacterController characterController;
-
+    //Allows the character to be able to move 
     private bool canMove = true;
 
     void Start()
     {
+        //Fully controls the characters input by allowing it to run, walk, crouch, jump etc.  
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -33,13 +35,14 @@ public class Movement : MonoBehaviour
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-
+        //Allows the player to be able to move in different directions and sprint when holdng the 'shift' button
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        //Controls the players jumping action and input by defining their jump power and speed
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
@@ -49,11 +52,13 @@ public class Movement : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
+        //Allows the player to stay on the ground by turning on gravity when moving
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
+        //Controls the players crouch speed when walking or running and is activated when pressing the 'R' button on the keyboard
         if (Input.GetKey(KeyCode.R) && canMove)
         {
             characterController.height = crouchHeight;
@@ -61,15 +66,17 @@ public class Movement : MonoBehaviour
             runSpeed = crouchSpeed;
         }
 
+        //Controls how fast the player is able to walk and run as well as the camera height
         else
         {
             characterController.height = defaultHeight;
-            walkSpeed = 6f;
-            runSpeed = 12f;
+            walkSpeed = 20f;
+            runSpeed = 30f;
         }
 
         characterController.Move(moveDirection * Time.deltaTime);
-
+        //Defines the camera speed and rotation, also how far the limit is for the player to look around
+        //The Camera moves around when the player uses the mouse
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
